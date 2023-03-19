bdc184b8b2ce3bfb1ac51ebe71924e0e7df96bd6
Anmol <kanmol21@tbc.edu.np>
2023-03-19 23:10:19 +0545

    practice done

--- app/src/main/java/com/example/lab_1/MainActivity.kt
+++ app/src/main/java/com/example/lab_1/MainActivity.kt
@@ -1,69 +1,44 @@
-/*
- * Copyright (C) 2020 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+package com.example.lab_1
 
-package com.example.diceroller
-
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
-import com.example.lab_1.R
-
+import android.widget.Toast
 /**
  * This activity allows the user to roll a dice and view the result
  * on the screen.
  */
-class MainActivity : AppCompatActivity() {
 
-    /**
-     * This method is called when the Activity is created.
-     */
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Find the Button in the layout
         val rollButton: Button = findViewById(R.id.button)
 
-        // Set a click listener on the button to roll the dice when the user taps the button
-        rollButton.setOnClickListener { rollDice() }
+        rollButton.setOnClickListener {
+            rollDice()
+        }
     }
 
-    /**
-     * Roll the dice and update the screen with the result.
-     */
     private fun rollDice() {
         // Create new Dice object with 6 sides and roll it
         val dice = Dice(6)
         val diceRoll = dice.roll()
 
+        val dice2 = Dice(6)
+        val diceRoll2 = dice2.roll()
         // Update the screen with the dice roll
         val resultTextView: TextView = findViewById(R.id.textView)
         resultTextView.text = diceRoll.toString()
+
+        val resultTextView2: TextView = findViewById(R.id.textView2)
+        resultTextView2.text = diceRoll2.toString()
     }
 }
 
-/**
- * Dice with a fixed number of sides.
- */
 class Dice(private val numSides: Int) {
 
-    /**
-     * Do a random dice roll and return the result.
-     */
     fun roll(): Int {
         return (1..numSides).random()
     }
