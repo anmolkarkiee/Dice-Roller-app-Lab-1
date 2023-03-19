05b6a000ac0c6a03f4f6bc517fd6b56f73dadc96
Anmol <kanmol21@tbc.edu.np>
2023-03-19 23:27:29 +0545

    updated readme

--- app/src/main/java/com/example/lab_1/MainActivity.kt
+++ app/src/main/java/com/example/lab_1/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+
 /**
  * This activity allows the user to roll a dice and view the result
  * on the screen.
